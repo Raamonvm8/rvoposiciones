3691218481fc9c746ed1315af691e442fa01f6cb
Ramón Valls Martin <ravama00@hotmail.com>
2025-12-29 23:21:56 +0000

    cookies

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,19 +7,40 @@ import { HttpClientModule } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { NgIf } from '@angular/common';
 
 
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, NgIf],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'rvoposiciones';
 
+  cookiesDecision: 'accepted' | 'rejected' | null = null;
+
+  ngOnInit() {
+    const decision = localStorage.getItem('cookiesDecision');
+    if (decision === 'accepted' || decision === 'rejected') {
+      this.cookiesDecision = decision as 'accepted' | 'rejected';
+    }
+  }
+
+  acceptCookies() {
+    this.cookiesDecision = 'accepted';
+    localStorage.setItem('cookiesDecision', 'accepted');
+    // Aquí puedes inicializar scripts de terceros, como Analytics
+  }
+
+  rejectCookies() {
+    this.cookiesDecision = 'rejected';
+    localStorage.setItem('cookiesDecision', 'rejected');
+    // Bloquear scripts de terceros si se usan
+  }
   
 }
 
